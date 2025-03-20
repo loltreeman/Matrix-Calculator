c19e5b734af9285f4507cb5ad74195a89419098c
loltreeman <naguio.greg@gmail.com>
2025-03-20 22:10:52 +0800

    I made sure to now show the Matrix Calculator

--- matrix-calculator-frontend/src/App.js
+++ matrix-calculator-frontend/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import MatrixCalculator from "./MatrixCalculator";
 
 function App() {
-    return <MatrixCalculator />;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-200">
+      <MatrixCalculator />
+    </div>
+  );
 }
 
 export default App;
